6824d6f1e345b279bddd370ce3b3495c5b7603d1
mss <mss@192.168.2.11>
2018-02-09 01:06:57 -0500

    partition

--- Lab-3.2/src/ArrayMethods2.java
+++ Lab-3.2/src/ArrayMethods2.java
@@ -4,10 +4,11 @@ public class ArrayMethods2 {
 	public static void main(String[] args) {
 		String[] list1 = {"aba", "ava", "dog", "egg", "shoe", "string", "xa"};
 		String[] list2 = {"banana", "computer", "hat", "saurabh", "train", "vex", "water", "xyz", "yeah", "zebra"};
-		merge(list1, list2);
-		/*for (String s : merge(list1, list2)) {
-			System.out.print(s + " ");
-		}*/
+		int[] list3 = {2, 1, 7, 8, 6, 3, 5, 4};
+		//merge(list1, list2);
+		for (int i : partition(list3)) {
+			System.out.print(i + " ");
+		}
 	}
 
 	public static String[] merge(String[] list1, String[] list2) {
@@ -46,20 +47,24 @@ public class ArrayMethods2 {
 		return list3;	
 	}
 	
-	public static int partition(int[] list) {
+	public static int[] partition(int[] list) {
 		/*	Write a method that partitions a list using the first element as the pivot. You should return the
 			integer position of the pivot in the newly partitioned array. The original array should be rearranged so that
 			all the elements before the pivot are less than or equal to the pivot and all the elements after the pivot are
 			greater than the pivot.
 		*/
-		int pivot = list[0];
-		int[] newList = new int[list.length];
-		for (int x = 1; x < list.length; x++) {
+		// Done with help from KC Ang's visual partition
+		int pivot = list[list.length - 1];
+		int y = 0;
+		for (int x = 0; x < list.length - 1; x++) {
 			if (list[x] < pivot) {
-				
+				y++;
+				int z = list[x];
+				list[x] = list[y];
+				list[y] = z;
 			}
 		}
-		return 0; //TEMP
+		return list;
 	}
 	
 }
