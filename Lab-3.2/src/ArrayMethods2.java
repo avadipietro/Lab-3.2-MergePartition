a9a5b31e504c18caf7fb73e14eb93e96b251963f
mss <mss@192.168.2.12>
2018-02-25 23:20:53 -0500

    a tad bit late

--- Lab-3.2/src/ArrayMethods2.java
+++ Lab-3.2/src/ArrayMethods2.java
@@ -1,71 +1,79 @@
+// Ava DiPietro
+
+import java.util.Arrays;
 
 public class ArrayMethods2 {
 	
 	public static void main(String[] args) {
 		String[] list1 = {"aba", "ava", "dog", "egg", "shoe", "string", "xa"};
 		String[] list2 = {"banana", "computer", "hat", "saurabh", "train", "vex", "water", "xyz", "yeah", "zebra"};
-		int[] list3 = {2, 1, 7, 8, 6, 3, 5, 4};
-		merge(list1, list2);
-		for (String i : merge(list1, list2)) {
-			System.out.print(i + " ");
-		}
+		int[] list3 = {4, 1, 7, 8, 6, 3, 5, 2};
+		
+		//Merge Test
+		long start = System.nanoTime();
+		String[] mergeResult = merge(list1, list2);
+		long end = System.nanoTime();
+		long time = end - start;
+		System.out.println("Merge test took: " + time + " nanoseconds");
+		System.out.println(Arrays.toString(mergeResult));
+		
+		//Partition Test
+		start = System.nanoTime();
+		int pivotFinalPos = partition(list3);
+		end = System.nanoTime();
+		time = end - start;
+		System.out.println("Partition test took: " + time + " nanoseconds");
+		System.out.println("Final Pivot Position: " + pivotFinalPos);
+		System.out.println(Arrays.toString(list3));
+		
 	}
 
 	public static String[] merge(String[] list1, String[] list2) {
-		/*	Pre-Condition: Both list1 and list2 contain Strings in alphabetical order from a to z.
-			Returns: An array of the two lists merged together. The result must be in alphabetical order.
-		*/
-		int l = 0;
+
 		int x = 0;
 		int y = 0;
+		int xcompy = 0;
 		String[] list3 = new String[list1.length + list2.length];
-		if (list1.length > list2.length) {
-			l = list2.length;
-		}
-		else {
-			l = list1.length;
-		}
-		for (int z = 0; z < (list3.length); z++) {
-			if (y < l && x < l) {
-				if (list1[x].compareTo(list2[y]) <= 0) {
-					list3[z] = list1[x];
-					x++;
-				}
-				else {
-					list3[z] = list2[y];
-					y++;
-				}
+		for (int z = 0; z < list3.length; z++) {
+			if (x >= list1.length) {
+				xcompy = 1;
 			}
-			else if (l == list1.length) {
-				list3[z] = list2[x];
+			else if (y >= list2.length) {
+				xcompy = -2;
+			}
+			else {
+				xcompy = list1[x].compareTo(list2[y]);
+			}
+			if (xcompy <= 0) {
+				list3[z] = list1[x];
 				x++;
 			}
 			else {
-				list3[z] = list1[x];
-				x++;
+				list3[z] = list2[y];
+				y++;
 			}
 		}
 		return list3;	
 	}
 	
-	public static int[] partition(int[] list) {
-		/*	Write a method that partitions a list using the first element as the pivot. You should return the
-			integer position of the pivot in the newly partitioned array. The original array should be rearranged so that
-			all the elements before the pivot are less than or equal to the pivot and all the elements after the pivot are
-			greater than the pivot.
-		*/
+	public static int partition(int[] list) {
 		// Done with help from KC Ang's visual partition
-		int pivot = list[list.length - 1];
-		int y = 0;
-		for (int x = 0; x < list.length - 1; x++) {
-			if (list[x] < pivot) {
-				y++;
-				int z = list[x];
-				list[x] = list[y];
-				list[y] = z;
+		
+		int i = 1;
+		int pivot = list[0];		
+		for (int j = 2; j < list.length; j++) {
+			if (list[j] < pivot) {
+				i++;
+				int m = list[i];
+				list[i] = list[j];
+				list[j] = m;
 			}
 		}
-		return list;
+		for (int a = 0; a < i; a++) {
+			list[a] = list[a + 1];
+		}
+		list[i] = pivot;
+		return i;
 	}
 	
 }
